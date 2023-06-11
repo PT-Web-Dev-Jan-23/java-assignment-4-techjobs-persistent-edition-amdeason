9c258b5a5313b79c76a4271624214c31bd164193
Andrew Deason <deasonam@gmail.com>
2023-06-11 16:34:40 -0500

    passing tests TaskOne, Task2

--- queries.sql
+++ queries.sql
@@ -1,6 +1,12 @@
 -- Part 1: Test it with SQL
+--    SELECT COLUMN_NAME, DATA_TYPE
+--    FROM INFORMATION_SCHEMA.COLUMNS
+--    WHERE
+--        TABLE_SCHEMA = "techjobs"
+--    AND TABLE_NAME = "job"
 
 -- Part 2: Test it with SQL
+    -- SELECT name FROM employer WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min = 3, max = 128, message = "Description must be between 3 and 128 characters")
+    private String description;
+
+    public Skill(){
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
